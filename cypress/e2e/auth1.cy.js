d7c7d985f53be5bdb0261247c98f3c89b066dc3c
Dudarev <DudarevMV@1forma.ru>
2024-01-22 10:18:32 +0300

    Update auth1.cy.js

--- cypress/e2e/auth1.cy.js
+++ cypress/e2e/auth1.cy.js
@@ -1,16 +1,15 @@
 
 describe('template spec', () => {
 
-  it('loginpopup', () => {
+  it('iphoneprice', () => {
 
-    const iphone = Cypress.env('IPHONE_PRICE');
+    const IPHONE = Cypress.env('IPHONE_PRICE');
 
     cy.visit('https://oscomp.ru/').clearAllCookies;
 
     cy.get('#title-search-input_fixed').click().type('Apple iPhone 15 256GB Pink').type('{enter}');
     cy.get(".section-gallery-wrapper__item._active").first().click();
-    cy.get(".price_value").contains(iphone);
-    
+    cy.get(".price_value").contains(IPHONE);
     
   })
 })
